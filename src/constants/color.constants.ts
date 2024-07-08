2106f8e0c5668ecaeef897ddb0bafa6c0e132b92
Forsythe <forsythe.dev@gmail.com>
2024-07-08 21:14:34 +0400

    Add constants

---
+++ src/constants/color.constants.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  primary: '#7551FF',
+  secondary: '#F97912',
+  light: '#EDEDED',
+  dark: '#0F0F0F',
+  contrast: '#151515',
+  border: 'rgba(255, 255, 255, .15)',
+  error: '#F67373'
+}
---
+++ src/constants/seo.constants.ts
@@ -0,0 +1,4 @@
+export const SITE_NAME = String(process.env.NEXT_PUBLIC_SITE_NAME)
+export const SITE_DESCRIPTION = String(process.env.NEXT_PUBLIC_SITE_DESCRIPTION)
+
+export const NO_INDEX = { robots: { index: false, follow: false } }
